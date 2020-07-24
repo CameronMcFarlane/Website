a03d3f489e7848aba12d1defd123830ed04ffb68
Cameron McFarlane <cmcfarlane.c@gmail.com>
2020-07-24 20:39:41 +0930

    Moved to a single prompt approach
    We now have one prompt which sends the commands to the terminal component.
    The terminal stores data about the current state of the prompt in an outputs array and we display all outputs above the prompt.

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terminal.component.css']
 })
 export class TerminalComponent implements OnInit {
+  
+  // The username of the user logged in
+  username: string = 'user';
+  // The current 'directory' we are in
+  directory: string = '/home/cam';
+  // The data to output
+  outputs: object[] = [];
 
+  // Constructors!
   constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  // Puts the cursor on the prompt when the terminal is clicked
+  focusPrompt() {
+    let prompts: HTMLCollection = document.getElementsByClassName('promptInput');
+    (<HTMLSpanElement>prompts[prompts.length - 1]).focus();
+  }
+
+  // Processes the command received from the prompt
+  processCommand(command: string) {
+    this.outputs.push({
+      username: this.username,
+      directory: this.directory,
+      command: command,
+      outputLines: []
+    })
   }
 
 }
