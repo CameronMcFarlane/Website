9012a985082767f12282751fcc1066f31ad7e262
Cameron McFarlane <cmcfarlane.c@gmail.com>
2020-07-27 18:17:18 +0930

    Added comments to Command

--- src/app/terminal/commands/command.ts
+++ src/app/terminal/commands/command.ts
@@ -1,11 +1,16 @@
 import { TerminalComponent } from '../terminal.component';
 
+/**
+ * An interface for all core terminal command functionality. 
+ */
 export interface Command {
-
-    // A list of alias for this command
+    // A list of keywords which will trigger the command
     alias: string[];
 
-    // Executes this command
+    /**
+     * Exectutes this command.
+     * @param terminal A reference to the terminal object.
+     * @param args A list of arguments given for this command.
+     */
     execute(terminal: TerminalComponent, args: string[]): void;
-
 }
