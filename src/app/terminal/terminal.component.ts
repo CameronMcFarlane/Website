52a9b17cd605c77bd31bf495932125f2bf583389
Cameron McFarlane <cmcfarlane.c@gmail.com>
2020-07-27 20:42:02 +0930

    Fixed Terminal not scrolling to bottom
    Implemented 'AfterViewChecked' to fix this.

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, AfterViewChecked } from '@angular/core';
 import { Command } from './commands/command';
 import { ClearCommand } from './commands/clear.command';
 import { LoginCommand } from "./commands/login.command";
@@ -9,7 +9,7 @@ import { LoginCommand } from "./commands/login.command";
   styleUrls: ['./terminal.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class TerminalComponent implements OnInit {
+export class TerminalComponent implements OnInit, AfterViewChecked {
   // The username of the user logged in
   username: string;
   // The current 'directory' we are in
@@ -29,19 +29,21 @@ export class TerminalComponent implements OnInit {
     this.username = "guest";
     this.directory = "/";
     this.outputs = [];
-
     this.commands = [
       new ClearCommand(),
       new LoginCommand()
     ]
   }
 
-  /**
-   * Angular Constructor. 
-   * Executes once all components are ready.
-   */
+  // Adds the first prompt to the output
   ngOnInit(): void {
     this.addPrompt();
+  }
+
+  // Ensure that the div always scrolls to the bottom
+  ngAfterViewChecked(): void {
+    let terminalText: HTMLCollection = document.getElementsByClassName('terminal-text');
+    terminalText[0].scrollTop = terminalText[0].scrollHeight;
   }
 
   /**
@@ -53,7 +55,7 @@ export class TerminalComponent implements OnInit {
       + '<span>:</span>'
       + '<span class="terminal-directory">' + this.directory + '</span>'
       + '<span>$&nbsp;</span>'
-    )
+    );
   }
 
   /**
@@ -69,30 +71,28 @@ export class TerminalComponent implements OnInit {
    * @param input The text received from the input
    */
   public processInput(input: string): void {
-    // Ensure that the div always scrolls to the bottom
-    let terminalText: HTMLCollection = document.getElementsByClassName('terminal-text');
-    terminalText[0].scrollTop = terminalText[0].scrollHeight;
-
     // Add command to the output
     this.outputs[this.outputs.length - 1] += input + '<br/>';
 
-    // Divide up the keyword and arguments
-    let keyword: string = input.trim().split(' ')[0];
-    let args: string[] = input.trim().split(' ').slice(1);
+    if (input.trim() != '') {
+      // Divide up the keyword and arguments
+      let keyword: string = input.trim().split(' ')[0];
+      let args: string[] = input.trim().split(' ').slice(1);
 
-    // Loop through all known command alias to find a match
-    let commandFound: boolean = false;
-    for (let command of this.commands) {
-      if (command.alias.includes(keyword)) {
-        command.execute(this, args);
-        commandFound = true;
+      // Loop through all known command alias to find a match
+      let commandFound: boolean = false;
+      for (let command of this.commands) {
+        if (command.alias.includes(keyword)) {
+          command.execute(this, args);
+          commandFound = true;
+        }
       }
-    }
 
-    // Print error if no valid command was found
-    if (!commandFound) {
-      this.outputs[this.outputs.length - 1] += 
-        keyword + ': command not found<br/>';
+      // Print error if no valid command was found
+      if (!commandFound) {
+        this.outputs[this.outputs.length - 1] += 
+          keyword + ': command not found<br/>';
+      }
     }
 
     // Adds prompt to screen
