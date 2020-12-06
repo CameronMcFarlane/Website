f1f68c54522d111827b1a39b2443a21464204172
Cameron McFarlane <cmcfarlane.c@gmail.com>
2020-12-06 17:10:08 +1030

    Moved SVG into separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TerminalComponent } from './terminal/terminal.component';
 import { InputComponent } from './terminal/input/input.component';
+import { GitHubLogoComponent } from './svg/github.logo.component';
+import { GitHubMarkComponent } from './svg/github.mark.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TerminalComponent,
-    InputComponent
+    InputComponent,
+    GitHubLogoComponent,
+    GitHubMarkComponent
   ],
   imports: [
     BrowserModule,
