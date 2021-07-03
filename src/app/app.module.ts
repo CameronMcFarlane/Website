9a6eeccf1cb1020ec08843b5d8238d68eef7f2d7
Cameron McFarlane <cmcfarlane.c@gmail.com>
2021-07-03 10:24:02 +0930

    Moved components into a folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TerminalComponent } from './terminal/terminal.component';
-import { InputComponent } from './terminal/input/input.component';
-import { GitHubLogoComponent } from './svg/github.logo.component';
-import { GitHubMarkComponent } from './svg/github.mark.component';
+import { AppComponent } from './components/app.component';
+import { TerminalComponent } from './components/terminal/terminal.component';
+import { InputComponent } from './components/terminal/input/input.component';
 import { DraggableDirective } from './directives/draggable.directive';
 
 @NgModule({
@@ -14,13 +11,10 @@ import { DraggableDirective } from './directives/draggable.directive';
     AppComponent,
     TerminalComponent,
     InputComponent,
-    GitHubLogoComponent,
-    GitHubMarkComponent,
     DraggableDirective
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
