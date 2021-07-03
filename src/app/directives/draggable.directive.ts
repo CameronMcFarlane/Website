ddc385967a94781fc9bb2e9db9788c09562f9d60
Cameron McFarlane <cmcfarlane.c@gmail.com>
2021-07-03 14:01:27 +0930

    Created DraggableDirective

--- src/app/directives/draggable.directive.ts
+++ src/app/directives/draggable.directive.ts
@@ -1,16 +1,48 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Directive, ElementRef, HostListener, Inject } from '@angular/core';
 
 @Directive({
   selector: '[draggable]'
 })
 export class DraggableDirective {
+  div: ElementRef;
+  document: Document;
+  startingMouseX: number;
+  startingMouseY: number;
 
-  constructor(element: ElementRef) {
-    element.nativeElement.style.backgroundColor = 'red';
+  constructor(@Inject(DOCUMENT) document: Document, element: ElementRef) {
+    this.div = element;
+    this.document = document;
   }
 
-  @HostListener('click') onClick() {
-    console.log('working');
+  @HostListener('mousedown', ['$event.clientX', '$event.clientY']) onDrag(mouseX: number, mouseY: number) {
+    this.startingMouseX = mouseX;
+    this.startingMouseY = mouseY;
+
+    this.document.onmousemove = (event) => {
+      event.preventDefault();
+      let newMouseX = this.startingMouseX - event.clientX;
+      let newMouseY = this.startingMouseY - event.clientY;
+      this.startingMouseX = event.clientX;
+      this.startingMouseY = event.clientY;
+      this.div.nativeElement.style.top = (this.div.nativeElement.offsetTop - newMouseY) + "px";
+      this.div.nativeElement.style.left = (this.div.nativeElement.offsetLeft - newMouseX) + "px";
+    };
+  }
+
+  // startDrag(event: any) {
+  //   console.log(event);
+  //   event.preventDefault();
+  //   let newMouseX = this.startingMouseX - event.clientX;
+  //   let newMouseY = this.startingMouseY - event.clientY;
+  //   this.startingMouseX = event.clientX;
+  //   this.startingMouseY = event.clientY;
+  //   this.div.nativeElement.style.top = (this.div.nativeElement.offsetTop - newMouseY) + "px";
+  //   this.div.nativeElement.style.left = (this.div.nativeElement.offsetLeft - newMouseX) + "px";
+  // }
+
+  @HostListener('mouseup') stopDrag() {
+    this.document.onmousemove = null;
   }
 
 }
